740b0b6ca54fd9036fa816cad252752ca9f92acd
yqli <yqli@oriondeMacBook-Pro.local>
2020-08-12 20:01:17 +0800

    dep

--- site/article/admin.py
+++
@@ -1,5 +0,0 @@
-﻿from django.contrib import admin
-from article.models import Article
-
-# Register your models here.
-admin.site.register(Article)
--- site/static/js/csrf.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by onion on 2016/4/30.
- */
- $.ajaxSetup({
-     beforeSend: function (xhr, settings) {
-          var csrftoken = $.cookie('csrftoken');
-          xhr.setRequestHeader("X-CSRFToken", csrftoken);
-     }
- });
