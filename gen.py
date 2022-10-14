ea9872fdf72a4334a38ed43104d9b8ddc8c56373
yongqiang li <yongqiangli@alumni.hust.edu.cn>
2022-10-14 15:59:44 +0800

    c

---
+++ gen.py
@@ -0,0 +1,19 @@
+#encoding=utf-8
+import os
+import re
+import sys
+import struct
+from itertools import groupby
+
+
+if __name__ == '__main__':
+    all_lst = open(sys.argv[1], 'r')
+    for line in all_lst:
+        line=line.strip()
+        tit = line.split('\t')[0]
+        url = line.split('\t')[1]
+        print("<tr>")
+        print("    <td width=\"150\" align=\"center\"></a></td>")
+        print("    <td width=\"800\">%s</td>"%(tit))
+        print("    <td width=\"200\"><a href=\"https://arxiv.org/pdf/"+url+".pdf\">pdf</a></td>")
+        print("</tr>")
